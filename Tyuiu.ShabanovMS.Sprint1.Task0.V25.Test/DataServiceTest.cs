fb5b1e74a839b864c029168feca39aafed035d5c
Tyuiu.ShabanovMS@outlook.com <shabanov2014@bk.ru>
2023-09-19 23:00:35 +0500

    Add project files.

---
+++ Tyuiu.ShabanovMS.Sprint1.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShabanovMS.Sprint1.Task0.V25.Lib;
+
+namespace Tyuiu.ShabanovMS.Sprint1.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidEcpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}

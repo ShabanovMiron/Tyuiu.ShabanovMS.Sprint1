8c43a95d3f052285d439a0ea22eff0113b7b9956
Tyuiu.ShabanovMS@outlook.com <shabanov2014@bk.ru>
2023-09-22 21:40:01 +0500

    Add Task2

---
+++ Tyuiu.ShabanovMS.Sprint1.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShabanovMS.Sprint1.Task2.V23.Lib;
+namespace Tyuiu.ShabanovMS.Sprint1.Task2.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 2;
+            double x = 60;
+            var res = ds.Calculate(a, x);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
---
+++ Tyuiu.ShabanovMS.Sprint1.Task2.V23/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShabanovMS.Sprint1.Task2.V23.Lib;
+
+//ЗАДАНИЕ
+//Написать программу, которая запрашивает у пользователя исходные данные, выполняет указанные расчёты и печатает результат на экране.
+//Формулировка задания: Задано количество минут. Перевести время в секунды.
+//Что пользователь вводит? Время в минутах (целое число)
+//Что программа печатает на экране? Время в секундах (целое число)
+
+namespace Tyuiu.ShabanovMS.Sprint1.Task2.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Шабанов М.С. | АСОиУб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт 1                                                                *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("*Выполнил: Шабанов Мирон Сергеевич | АСОиУб-23-1                          *");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Задано количество минут. Перевести время в секунды.                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a;
+            double x = 60;
+
+            Console.WriteLine("Введите количество минут:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            Console.WriteLine("Количество секунд в " + a + " минутах равно " + ds.Calculate(a, x));
+            Console.ReadKey();
+        }
+
+    }
+}

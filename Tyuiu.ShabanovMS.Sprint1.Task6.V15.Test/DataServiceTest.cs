fa8cba05846fda18f56955f3f40799277865d802
Tyuiu.ShabanovMS@outlook.com <shabanov2014@bk.ru>
2023-09-29 00:33:06 +0500

    Add Task6

---
+++ Tyuiu.ShabanovMS.Sprint1.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShabanovMS.Sprint1.Task6.V15.Lib;
+namespace Tyuiu.ShabanovMS.Sprint1.Task6.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string value = "Больше букв чем символов!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"; 
+            bool res = ds.CheckLettersCount(value);
+            bool wait = true;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.ShabanovMS.Sprint1.Task6.V15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShabanovMS.Sprint1.Task6.V15.Lib;
+namespace Tyuiu.ShabanovMS.Sprint1.Task6.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Шабанов М.С. | АСОиУб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт 1                                                                *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("*Выполнил: Шабанов Мирон Сергеевич | АСОиУб-23-1                          *");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                   * ");
+            Console.WriteLine("* Дано значение температуры в градусах Фаренгейта. Определить значение    *");
+            Console.WriteLine("* этой же температуры в градусах Цельсия.                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string value = Console.ReadLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CheckLettersCount(value));
+            Console.ReadLine();
+        }
+    }
+}
